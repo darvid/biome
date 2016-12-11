808ceab4eb3d083986a82eacb25e7b1713926ab2
David Gidwani <david.gidwani@gmail.com>
2016-12-11 13:16:34 -0500

    fixed error on import in Python 3.3, cleaned up packaging

--- setup.py
+++ setup.py
@@ -8,44 +8,65 @@ Links
 * `Documentation <http://biome.readthedocs.org>`_
 
 """
+import io
 import sys
 
 import setuptools
 
-import versioneer
+
+__all__ = ('setup',)
 
 
-install_requires = ["attrdict"]
-if sys.version_info[:2] < (3, 4):
-    install_requires.append("pathlib")
+def readme():
+    with io.open('README.rst') as fp:
+        return fp.read()
 
 
-setuptools.setup(
-    name="biome",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    description="Painless access to namespaced environment variables",
-    license="BSD",
-    author="David Gidwani",
-    author_email="david.gidwani@gmail.com",
-    url="https://github.com/darvid/biome",
-    download_url="https://github.com/darvid/biome/tarball/0.1",
-    keywords="conf config configuration environment",
-    py_modules=["biome"],
-    install_requires=install_requires,
-    tests_require=["pytest"],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-)
+def setup():
+    """Package setup entrypoint."""
+    install_requirements = ["attrdict"]
+    if sys.version_info[:2] < (3, 4):
+        install_requirements.append("pathlib")
+    setup_requirements = ['six', 'setuptools>=17.1', 'setuptools_scm']
+    needs_sphinx = {
+        'build_sphinx',
+        'docs',
+        'upload_docs',
+    }.intersection(sys.argv)
+    if needs_sphinx:
+        setup_requirements.append('sphinx')
+    setuptools.setup(
+        author="David Gidwani",
+        author_email="david.gidwani@gmail.com",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
+            "Topic :: Software Development",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+        description="Painless access to namespaced environment variables",
+        download_url="https://github.com/darvid/biome/tarball/0.1",
+        install_requires=install_requirements,
+        keywords="conf config configuration environment",
+        license="BSD",
+        long_description=readme(),
+        name="biome",
+        package_dir={'': 'src'},
+        packages=setuptools.find_packages('./src'),
+        setup_requires=setup_requirements,
+        tests_require=["pytest"],
+        url="https://github.com/darvid/biome",
+        use_scm_version=True,
+    )
+
+
+if __name__ == '__main__':
+    setup()
--- src/biome/__init__.py
+++ src/biome/__init__.py
@@ -1,4 +1,6 @@
 """Provides painless access to namespaced environment variables."""
+from __future__ import absolute_import
+
 import ast
 import os
 import pathlib
@@ -7,16 +9,18 @@ import sys
 import attrdict
 
 
-__all__ = ("EnvironmentError", "Habitat")
+__all__ = ('EnvironmentError', 'Habitat')
 
+
+package_name = 'biome'
 
 # Create a reference to the module so it doesn't get deleted
 # See http://stackoverflow.com/a/5365733/211772
-_module_ref = sys.modules[__name__]
+_module_ref = sys.modules[package_name]
 
 
 def _sanitize_prefix(prefix):
-    return prefix.upper().rstrip("_")
+    return prefix.upper().rstrip('_')
 
 
 class EnvironmentError(KeyError):
@@ -28,12 +32,12 @@ class EnvironmentError(KeyError):
     """
 
     @classmethod
-    def not_found(cls, prefix, name):
-        return cls("'%s_%s' does not exist in the environment" %
+    def not_found(cls, prefix, name):  # noqa: D102
+        return cls('"%s_%s" does not exist in the environment' %
                    (prefix, name.upper()))
 
 
-class Habitat(attrdict.AttrDict):
+class Habitat(attrdict.AttrDict):  # noqa: D205,D400
     """Provides attribute/map style access to a set of namespaced
     environment variables.
 
@@ -46,19 +50,18 @@ class Habitat(attrdict.AttrDict):
 
     """
 
-    def __new__(cls, prefix):
+    def __new__(cls, prefix):  # noqa: D102
         if isinstance(prefix, attrdict.AttrDict):
             return prefix
         return super(Habitat, cls).__new__(cls, prefix)
 
-    def __init__(self, prefix):
-        self._setattr("_prefix", _sanitize_prefix(prefix))
+    def __init__(self, prefix):  # noqa: D102
+        self._setattr('_prefix', _sanitize_prefix(prefix))
         super(Habitat, self).__init__(self.get_environ(self._prefix))
 
     @classmethod
     def get_environ(cls, prefix):
-        """Retrieves a list of environment variables and their values
-        for a given namespace.
+        """Retrieves environment variables from a namespace.
 
         Args:
             prefix (str): The prefix, without a trailing underscore.
@@ -69,7 +72,7 @@ class Habitat(attrdict.AttrDict):
         """
         return ((key[len(prefix) + 1:], value)
                 for key, value in os.environ.items()
-                if key.startswith("%s_" % prefix))
+                if key.startswith('%s_' % prefix))
 
     def get(self, name, default=None):
         """A more explicit alternative to attribute or item access.
@@ -199,8 +202,7 @@ class Habitat(attrdict.AttrDict):
         return list(self[name])
 
     def get_path(self, name, default=None):
-        """Retrieves an environment variable as a ``pathlib.Path``
-        object.
+        """Retrieves an environment variable as a filesystem path.
 
         Requires the `pathlib`_ library if using Python <= 3.4.
 
@@ -236,10 +238,10 @@ class Habitat(attrdict.AttrDict):
         """
         super(Habitat, self).update(self.get_environ(self._prefix))
 
-    def __contains__(self, name):
+    def __contains__(self, name):  # noqa: D105
         return super(Habitat, self).__contains__(name.upper())
 
-    def __getitem__(self, name):
+    def __getitem__(self, name):  # noqa: D105
         name = name.upper()
         try:
             value = super(Habitat, self).__getitem__(name)
@@ -247,7 +249,7 @@ class Habitat(attrdict.AttrDict):
             raise EnvironmentError.not_found(self._prefix, name)
         try:
             # Attempt to parse value as a Python literal
-            if value.lower() in ("true", "false"):
+            if value.lower() in ('true', 'false'):
                 value = value.title()
             value = ast.literal_eval(value)
             if isinstance(value, dict):
@@ -258,40 +260,42 @@ class Habitat(attrdict.AttrDict):
             # * value contains the default OS path separator
             # * the substring 'PATH', 'DIR', or 'FILE' in the var name
             if (os.path.sep in value and
-                    "PATH" in name_upper or
-                    "DIR" in name_upper or
-                    "FILE" in name_upper):
+                    'PATH' in name_upper or
+                    'DIR' in name_upper or
+                    'FILE' in name_upper):
                 value = pathlib.Path(value)
         return value
 
-    def __repr__(self):  # pragma: no cover
-        return u"<{}({!r})>".format(self.__class__.__name__, self._prefix)
+    def __repr__(self):  # noqa: D105
+        return '<{}({!r})>'.format(self.__class__.__name__, self._prefix)
 
 
 class Biome(attrdict.AttrDict):
     """Provides utilities for accessing environment variables."""
 
     def __getattr__(self, name):
-        if name == "_lib":
+        if name in ('_lib', '__package__'):
             return _module_ref
+        elif name in '__loader__':
+            return __loader__
         try:
             return self[name.upper()]
         except KeyError:
-            if name.startswith("_"):
+            if name.startswith('_'):
                 raise
             name = _sanitize_prefix(name)
-            if name not in self and not name.startswith("_"):
+            if name not in self and not name.startswith('_'):
                 self[name] = _module_ref.Habitat(name)
             return self[name]
 
     def __repr__(self):  # pragma: no cover
-        return "<{}({!r})>".format(
+        return '<{}({})>'.format(
             self.__class__.__name__,
             dict.__repr__(self))
 
 
-sys.modules[__name__] = Biome()
-for prop in ("file", "name"):
-    object.__setattr__(sys.modules[__name__], "__%s__" % prop,
-                       locals()["__%s__" % prop])
-sys.modules["%s._lib" % __name__] = _module_ref
+sys.modules[package_name] = Biome()
+for prop in ('file', 'name'):
+    object.__setattr__(sys.modules[package_name], '__%s__' % prop,
+                       locals()['__%s__' % prop])
+sys.modules['%s._lib' % package_name] = _module_ref
